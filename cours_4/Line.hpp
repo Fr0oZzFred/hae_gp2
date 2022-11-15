b8ac3c48d72e60bc611cfeef00a91ab02b78aae8
Fr0oZzFred <78353908+Fr0oZzFred@users.noreply.github.com>
2022-11-15 17:07:54 +0100

    catmull + line

---
+++ cours_4/Line.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+class Line {
+public:
+
+	void draw(sf::RenderWindow& win) {
+		sf::VertexArray vb(sf::LinesStrip);
+		for (auto p : baked) {
+			sf::Vertex vtx(p, sf::Color::Cyan);
+			vb.append(vtx);
+		}
+		win.draw(vb);
+	}
+
+
+	void setPoints(std::vector <sf::Vector2f>& p, float tResolution = 0.1f) {
+		auto get = [&p](int idx) {
+			if (idx < 0) idx = 0;
+			if (idx >= p.size()) idx = p.size() - 1;
+			return p[idx];
+		};
+		baked.clear();
+		double steps = std::ceil(1.0 / tResolution);
+		double cstep = 0.0;
+		for (int i = 0; i < p.size() - 1; i++) {
+			auto p0 = get(i - 1);
+			auto p1 = get(i);
+			auto p2 = get(i + 1);
+			auto p3 = get(i + 2);
+			cstep = 0.0;
+			for (int i = 0; i < steps; i++) {
+				baked.push_back(Catmull::polynom2(p0, p1, p2, p3, cstep));
+				cstep += tResolution;
+			}
+		}
+	};
+private:
+	std::vector <sf::Vector2f> baked;
+
+	sf::CircleShape CreateRec(sf::Vector2f p) {
+		sf::CircleShape p0(4);
+		p0.setOrigin(2,2);
+		p0.setFillColor(sf::Color::Green);
+		p0.setPosition(p);
+		return p0;
+	};
+};
--- cours_4/main.cpp
+++ cours_4/main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
-#include "Lib.hpp"
 #include <SFML/Graphics.hpp>
+#include "Lib.hpp"
+#include "Catmull.hpp"
+#include "Line.hpp"
+static const sf::String projectName = "Eevee";
 
-int main() {
-    const sf::String projectName = "Eevee";
+int testSFML() {
+    sf::ContextSettings(0, 0, 2);
     sf::RenderWindow window(sf::VideoMode(1920, 1080), projectName, sf::Style::Fullscreen);
+    window.setVerticalSyncEnabled(true);
     bool fullscreen = true;
 
     sf::Texture texture;
@@ -13,6 +17,37 @@ int main() {
     shape.setTexture(&texture);
     //shape.setFillColor(sf::Color::Magenta);
     int speed = 200;
+
+
+    sf::RectangleShape line(sf::Vector2f(500, 1));
+    line.setPosition(window.getSize().x * .5, window.getSize().y * .5);
+    int rotSpeed = 10;
+
+
+    float t = 0;
+
+    std::vector<sf::RectangleShape> catmullPoints;
+
+    auto plot = [&](sf::Vector2f p) {
+        sf::RectangleShape p0(sf::Vector2f(16, 16));
+        p0.setOrigin(8, 8);
+        p0.setFillColor(sf::Color::White);
+        p0.setOutlineColor(sf::Color::Green);
+        p0.setOutlineThickness(2);
+        p0.setRotation(45);
+        p0.setPosition(p);
+        catmullPoints.push_back(p0);
+    };
+
+    plot(sf::Vector2f(1500, 100));
+    plot(sf::Vector2f(50, 500));
+    plot(sf::Vector2f(1900, 300));
+    plot(sf::Vector2f(1000, 900));
+
+    plot(sf::Vector2f(0, 0));
+
+
+
 
     sf::Clock deltaClock;
     while (window.isOpen()) {
@@ -49,11 +84,49 @@ int main() {
 
         shape.setPosition(pos);
 
+        line.setRotation(line.getRotation() + rotSpeed * dt.asSeconds());
+
+        t -= sf::Keyboard::isKeyPressed(sf::Keyboard::Left) * .01;
+        t += sf::Keyboard::isKeyPressed(sf::Keyboard::Right) * .01;
+        if (t < 0) t = 0;
+        if (t > 1) t = 1;
+
+
+        catmullPoints[4].setPosition(
+            Catmull::polynom2(
+                catmullPoints[0].getPosition(),
+                catmullPoints[1].getPosition(),
+                catmullPoints[2].getPosition(),
+                catmullPoints[3].getPosition(),
+                t
+            ));
+        catmullPoints[4].setOutlineColor(sf::Color::Magenta);
+
+        std::vector<sf::Vector2f> points;
+        points.push_back(catmullPoints[0].getPosition());
+        points.push_back(catmullPoints[1].getPosition());
+        points.push_back(catmullPoints[2].getPosition());
+        points.push_back(catmullPoints[3].getPosition());
+
+
+        Line catmullLine;
+
+        catmullLine.setPoints(points, 0.00001);
+
+
+
         window.clear();
         window.draw(shape);
+        window.draw(line);
+        for(auto p : catmullPoints)
+            window.draw(p);
+        catmullLine.draw(window);
         window.display();
 
     }
+};
 
+int main() {
+    testSFML();
     return 0;
 };
