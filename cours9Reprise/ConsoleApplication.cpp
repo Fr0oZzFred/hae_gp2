d14a8d18c8a674e7d78c3e23d39ee08aa763b1c5
Frédéric Deiber <frederic.deiber@gmail.com>
2022-12-14 17:20:53 +0100

    correction dijkstra

---
+++ cours9Reprise/ConsoleApplication.cpp
@@ -0,0 +1,212 @@
+#include "Lib.hpp"
+
+#include <filesystem>
+#include <iostream>
+#include <string>
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include "Catmull.hpp"
+#include "Line.hpp"
+#include "Entities.hpp"
+#include "Game.hpp"
+#include "Part.hpp"
+#include "CmdFile.hpp"
+#include "World.hpp"
+#include "imgui.h"
+#include <unordered_map>
+#include <algorithm>
+#include "Dijkstra.hpp"
+
+
+using namespace sf;
+class Player : public Entity {
+public:
+	Player() : Entity( sf::Vector2f(50,100) , new RectangleShape(sf::Vector2f(24,64)) ){
+		auto rect  = (sf::RectangleShape*) this->shp;
+		rect->setOrigin(12, 64);
+		rect->setFillColor(sf::Color::Red);
+		rect->setOutlineColor(sf::Color::Green);
+		rect->setOutlineThickness(2);
+	}
+};
+
+/*
+namespace std {
+	template <> struct hash<sf::Vector2i> {
+		std::size_t operator()(const sf::Vector2i& k) const {
+			using std::hash;
+			return std::size_t((k.y << 16) | k.x);
+		}
+	};
+}*/
+
+
+
+static Dijkstra dij;
+static Player * player = nullptr;
+
+void printGraph(BaseGraph& g, sf::VertexArray& points) {
+	int hcell = Cst::CELL_SIZE >> 1;
+	points.clear();
+	for (auto& p : g) {
+		points.append(sf::Vertex(sf::Vector2f(p.first.x * Cst::CELL_SIZE + hcell, p.first.y * Cst::CELL_SIZE + hcell), sf::Color::Red));
+	}
+};
+void printWayGraph(SpeedingWay& g, sf::VertexArray& points) {
+	int hcell = Cst::CELL_SIZE >> 1;
+	points.clear();
+	for (auto& p : g) {
+		points.append(sf::Vertex(sf::Vector2f(p.x * Cst::CELL_SIZE + hcell, p.y * Cst::CELL_SIZE + hcell), sf::Color::Yellow));
+	}
+};
+
+void testSFML(){
+	player = new Player();
+	std::cout << std::filesystem::current_path() << std::endl;
+
+	sf::ContextSettings settings(0,0,2);
+	sf::RenderWindow window(sf::VideoMode(Game::WIDTH, Game::HEIGHT), "SFML works!");
+	window.setFramerateLimit(60);
+	window.setVerticalSyncEnabled(true);
+
+	time_t lastModificationTime = {};
+	double frameStart = 0;
+	double frameEnd = 0.0015f;
+
+	ImGui::SFML::Init(window);
+
+	sf::VertexArray points;
+	sf::VertexArray way;
+	points.setPrimitiveType(Points);
+
+	auto bricks = CmdFile::loadScript("res/save.txt");
+	for( auto & c : bricks)
+		if (c.id == CmdId::Wall)
+			world.statics.push_back(sf::Vector2i(c.x, c.y));
+	world.mkGraphics();
+	std::cout << "bricks read :" << std::to_string(bricks.size());
+	
+	while (window.isOpen()) { // ONE FRAME
+		double dt = frameEnd - frameStart;
+		frameStart = Lib::getTimestamp();
+		sf::Event event;
+		while (window.pollEvent(event)) { // ONE EVENT
+			
+			ImGui::SFML::ProcessEvent(window, event);
+			player->onEvent(event);
+			if (event.type == sf::Event::Closed)
+				window.close();
+			if (event.type == sf::Event::MouseButtonPressed) {
+				int mcx = (int)event.mouseButton.x / Cst::CELL_SIZE;
+				int mcy = (int)event.mouseButton.y / Cst::CELL_SIZE;
+				//add block to world's statics
+				//add test against statics in world
+				world.poke(mcx, mcy);
+				std::vector<Cmd> cmds;
+				for(auto &s : world.statics)
+					cmds.push_back({ CmdId::Wall,s.x,s.y });
+				CmdFile::saveScript("res/save.txt",cmds);
+			}
+
+			if (event.type == sf::Event::KeyReleased) {
+
+				if (event.key.code == sf::Keyboard::C) {
+					std::unordered_map<sf::Vector2i, bool> g;
+					dij.setGraph( g );
+				}
+				if( event.key.code == sf::Keyboard::G){
+					//creer un graph de toute les cases valides
+
+					//std::vector<> g <- contient toutes les cases (cx,cy) valides de notre jeu
+					//std::unordered_map<> g <- contient toutes les cases (cx,cy) valides de notre jeu
+
+					// le passer a "quelque chose" qui sera notre futur dijkstra
+					//appeler afficher graph( g )
+					
+					std::unordered_map<sf::Vector2i, bool> g;
+					for(int y = 0; y <  1 +Game::HEIGHT / Cst::CELL_SIZE; ++y)
+					for(int x = 0; x <  1 +Game::WIDTH / Cst::CELL_SIZE; ++x){
+						if( !world.collides(x,y))
+							g[sf::Vector2i(x, y)] = true;
+					}
+					
+					printGraph(g,points);
+
+					dij.setGraph(g);
+				}
+
+				if (event.key.code == sf::Keyboard::D) {
+					std::unordered_map<sf::Vector2i, bool> g;
+					for (int y = 0; y < 1 + Game::HEIGHT / Cst::CELL_SIZE; ++y)
+						for (int x = 0; x < 1 +Game::WIDTH / Cst::CELL_SIZE; ++x) {
+							if (!world.collides(x, y))
+								g[sf::Vector2i(x, y)] = true;
+						}
+					dij.setGraph(g);
+
+					if( false )
+					{
+						dij.d[sf::Vector2i(1, 3)] = 1000;
+						dij.d[sf::Vector2i(1, 0)] = 1;
+						VertexList q;
+						q.push_back(sf::Vector2i(1, 3));
+						q.push_back(sf::Vector2i(1, 0));
+						int idx = dij.findMin(q);
+						int here = 0; // idx shoudl be 1
+					}
+
+					if(false)
+					{
+						dij.d[sf::Vector2i(1, 0)] = 66;
+						dij.updateDist(sf::Vector2i(1, 0), sf::Vector2i(0, 0));
+					}
+
+					dij.build(sf::Vector2i(player->cx, player->cy));
+					printGraph(g, points);
+					printWayGraph(dij.way, way);
+				}
+			}
+		}
+		
+		player->update();
+		bool t = true;
+		ImGui::SFML::Update(window, sf::Time( sf::seconds(dt)));
+		{
+			using namespace ImGui;
+			//ImGui::ShowDemoWindow(&t);
+
+			ImGui::Begin("Debug", &t);
+
+			player->im();
+
+			if( TreeNode("dijkstra")){
+				dij.im();
+				TreePop();
+			}
+
+			ImGui::End();
+		}
+		window.clear();
+		
+		player->draw(window);
+		world.draw(window);
+		window.draw(points);
+		window.draw(way);
+
+		ImGui::EndFrame();
+		ImGui::SFML::Render(window);
+		window.display();
+		frameEnd = Lib::getTimestamp();
+	}
+	ImGui::SFML::Shutdown();
+}
+
+int WinMain() {
+	testSFML();
+	return 0;
+}
+
+int main(){
+	testSFML();
+	return 0;
+}
---
+++ cours9Reprise/Dijkstra.hpp
@@ -0,0 +1,133 @@
+#pragma once
+
+#include <unordered_map>
+#include <algorithm>
+
+#include "SFML/System/Vector2.hpp"
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+namespace std {
+	template <> struct hash<sf::Vector2i> {
+		inline std::size_t operator()(const sf::Vector2i& k) const {
+			using std::hash;
+			return std::size_t((k.y * 16 * 1024) + k.x);
+		};
+	};
+}
+
+typedef std::unordered_map<sf::Vector2i, bool>			BaseGraph;
+typedef std::unordered_map<sf::Vector2i, float>			DistanceGraph;
+typedef std::unordered_map<sf::Vector2i, sf::Vector2i>	AssocGraph;
+typedef std::vector<sf::Vector2i>						VertexList;
+typedef std::vector<sf::Vector2i>						SpeedingWay;
+
+class Dijkstra{
+public:
+	sf::Vector2i	start = sf::Vector2i(-1,-1);
+	BaseGraph		g;//les sommets
+	DistanceGraph	d;//les distances
+	AssocGraph		pred;//a tout sommet S1 -> S2, qui est le predecesseur dans le chemin S1 -> Start => s2 
+	SpeedingWay		way;
+	sf::Vector2i end = sf::Vector2i(0, 0);
+
+	Dijkstra() {
+	};
+
+	void setGraph(BaseGraph& _g){
+		g = _g;
+		d.clear();
+		pred.clear();
+		way.clear();
+	}
+
+	void init(sf::Vector2i _start) {
+		start = _start;
+		for (auto& s : g)
+			d[s.first] = 1024 * 1024;
+		d[start] = 0;
+	};
+
+	//Q ?
+	int findMin(VertexList& q) {
+		float mini = 1024 * 1024;
+		int vertexIdx = -1;
+		int idx = 0;
+		for (sf::Vector2i & s : q) {
+			if (d[s] < mini) {
+				mini = d[s];
+				vertexIdx = idx;
+			}
+			idx++;
+		}
+		return vertexIdx;
+	};
+
+	inline float heur(sf::Vector2i s1, sf::Vector2i s2) {
+		float dx = s2.x - s1.x;
+		float dy = s2.y - s1.y;
+		return sqrt(dx * dx + dy * dy);
+	};
+
+	void updateDist(sf::Vector2i s1, sf::Vector2i s2) {
+		float ndist = d[s1] + heur(s1, s2);
+		if (d[s2] > ndist) {
+			d[s2] = ndist;
+			pred[s2] = s1;
+		}
+	};
+
+	void build(sf::Vector2i _start) {
+		init(_start);
+		VertexList q;
+		for (auto& p : g)
+			q.push_back(p.first);
+
+		sf::Vector2i tldr[4] = {
+			sf::Vector2i(0,1),
+			sf::Vector2i(0,-1),
+			sf::Vector2i(1,0),
+			sf::Vector2i(-1,0),
+		};
+
+		while (!q.empty()) {
+			int s1Idx = findMin(q);
+			if (s1Idx == -1) break;
+
+			sf::Vector2i s1 = q[s1Idx];
+			q.erase(q.begin() + s1Idx);
+
+			for (int i = 0; i < 4; ++i) {
+				auto s2 = tldr[i] + s1;
+				if (g.find(s2) == g.end()) continue;
+				updateDist(s1, s2);
+			}
+		}
+		sf::Vector2i tmp = pred[end];
+		while (tmp != _start) {
+			way.push_back(tmp);
+			tmp = pred[tmp];
+		}
+		way.push_back(_start);
+	};
+
+	void im(){
+		using namespace ImGui;
+		Value("start x", start.x);
+		Value("start y", start.y);
+		DragInt2("Weight", &end.x);
+		if( TreeNode("Preds")){
+			int idx = 0;
+			for(auto & p: pred){
+				if( TreeNode(std::to_string(idx).c_str())){
+					ImGui::LabelText("v","%d,%d -> %d,%d", p.first.x,p.first.y, p.second.x,p.second.y );
+					TreePop();
+				}
+				idx++;
+			}
+			TreePop();
+		}
+	}
+
+	//
+};
