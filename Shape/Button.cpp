6df95ee443285836c058c2be48b91c6123cd4203
Fr0oZzFred <78353908+Fr0oZzFred@users.noreply.github.com>
2022-12-25 12:08:11 +0100

    Button text

--- Shape/Button.cpp
+++ Shape/Button.cpp
@@ -1,44 +1,70 @@
 #include "Button.hpp"
 #include "UI.hpp"
 
-Button::Button(sf::Vector2f pixelPos) {
-	setPixelPos(pixelPos);
-	box = new sf::RectangleShape(sf::Vector2f(200.0f, 50.0f));
-	box->setFillColor(sf::Color::White);
-};
 void Button::draw(sf::RenderWindow& window) {
 	window.draw(*box);
+	window.draw(text);
 };
 void Button::update() {
-	box->setFillColor(sf::Color(r * 255.0f, g * 255.0f, b * 255.0f));
+	box->setFillColor(sf::Color(buttonColor[0] * 255.0f, buttonColor[1] * 255.0f, buttonColor[2] * 255.0f));
+	text.setFillColor(sf::Color(textColor[0] * 255.0f, textColor[1] * 255.0f, textColor[2] * 255.0f)); 
 	UiElement::baseUpdate();
 	box->setPosition(xx, yy);
+	text.setPosition(
+		xx + offsetX * Game::CELL_SIZE,
+		yy + offsetY * Game::CELL_SIZE);
 };
 void Button::im() {
 	using namespace ImGui;
-	DragInt2("cx cy", &cx);
-	DragFloat2("rx ry", &rx);
-	Value("pos x", xx);
-	Value("pos y", yy);
-	ImGui::ColorPicker3("Color", &r);
-	static char _name[64] = "";
-	if(ImGui::InputText("Name", _name, IM_ARRAYSIZE(_name), ImGuiInputTextFlags_CharsNoBlank)) return;
-	if (ImGui::Button("Rename")) Lib::MemcpyRec(name, _name, 64);
-	if (ImGui::Button("Remove")) ui.remove(this);
+	if (ImGui::TreeNode("Main")) {
+		ImGui::InputText("Name", _name, IM_ARRAYSIZE(_name), ImGuiInputTextFlags_CharsNoBlank);
+		if (ImGui::Button("Rename")) Lib::MemcpyRec(name, _name, 64);
+		if (ImGui::Button("Remove")) ui.remove(this);
+		TreePop();
+	}
+	if (ImGui::TreeNode("Button")) {
+		DragInt2("cx cy", &cx);
+		DragFloat2("rx ry", &rx);
+		Value("pos x", xx);
+		Value("pos y", yy);
+		if (ImGui::TreeNode("ButtonColor")) {
+			ImGui::ColorPicker3("ButtonColor", &buttonColor[0]);
+			TreePop();
+		}
+		TreePop();
+	}
+	if (ImGui::TreeNode("Text")) {
+		DragFloat2("Offset", &offsetX);
+		Value("pos x", xx + offsetX * Game::CELL_SIZE);
+		Value("pos y", yy + offsetY * Game::CELL_SIZE);
+		DragFloat("Font Size", &fontSize);
+		if (ImGui::InputText("Content", content, IM_ARRAYSIZE(content), ImGuiInputTextFlags_CharsNoBlank)) {
+			text.setString(content);
+		}
+		if (ImGui::TreeNode("TextColor")) {
+			ImGui::ColorPicker3("TextColor", &textColor[0]);
+			TreePop();
+		}
+		TreePop();
+	}
 };
 void Button::save(FILE* file) {
 	fprintf(file,
-		"%s %i %i %f %f %f %f %f \n"
-		,name, cx, cy, rx, ry,
-		r, g, b
+		"%s %i %i %f %f %f %f %f %f %f %f %s %f %f %f\n",
+		name, cx, cy, rx, ry,
+		buttonColor[0], buttonColor[1], buttonColor[2],
+		offsetX, offsetY, fontSize, content,
+		textColor[0], textColor[1], textColor[2]
 	);
 };
 void Button::load(FILE* file) {
-	static char _name[64] = {};
 	fscanf_s(file,
-		"%s %i %i %f %f %f %f %f \n",
+		"%s %i %i %f %f %f %f %f %f %f %f %s %f %f %f\n",
 		_name, 64, &cx, &cy, &rx, &ry,
-		&r, &g, &b
+		&buttonColor[0], &buttonColor[1], &buttonColor[2],
+		&offsetX, &offsetY, &fontSize, &content, 128,
+		&textColor[0], &textColor[1], &textColor[2]
 	);
 	Lib::MemcpyRec(name, _name, 64);
+	text.setString(content);
 };
--- Shape/Button.hpp
+++ Shape/Button.hpp
@@ -5,21 +5,34 @@
 
 class Button : public UiElement {
 public:
+	//Button
 	sf::RectangleShape* box = nullptr;
-	sf::Font text;
+	float buttonColor[3] = { 1.0f, 1.0f ,1.0f };
 
-	float r = 1, g = 1, b = 1;
+	//Text
+	sf::Font font;
+	sf::Text text;
+	float textColor[3] = { 0.0f, 0.0f ,0.0f };
+	float fontSize = 10;
+	float offsetX = 0.3f, offsetY = 0.3f;
+	char content[128]{};
+	
 
 	Button() {
+		if(!font.loadFromFile("Ressources/depixel/DePixelBreitFett.ttf")) throw "Font not Found";
+		text.setFont(font);
+		text.setString("NewButton");
+		text.setCharacterSize(fontSize);
 		Lib::Memcpy(name, "NewButton", 10);
 		setPixelPos(sf::Vector2f(1920/2, 1080/2));
 		box = new sf::RectangleShape(sf::Vector2f(200.0f, 50.0f));
 	};
-	Button(sf::Vector2f pixelPos);
 
 	void draw(sf::RenderWindow& window);
 	void update();
 	void im();
 	void save(FILE* file);
 	void load(FILE* file);
+private:
+	char _name[64] = {};
 };
